d2b7ce6881ea09a0e0e011299f5299ba45fdc988
Alex Lockwood <alexjlockwood@gmail.com>
2016-12-31 00:45:38 -0500

    add debug mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,50 @@
-import { Component } from '@angular/core';
-import { CanvasComponent } from './canvas/canvas.component';
-import { PointListComponent } from './pointlist/pointlist.component';
+import { Component, OnInit } from '@angular/core';
 import { VectorLayer, PathLayer } from './scripts/models';
 import * as SvgLoader from './scripts/svgloader';
 import { SvgPathData } from './scripts/svgpathdata';
 
+
+const debugMode = true;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private startVectorLayer: VectorLayer;
   private previewVectorLayer: VectorLayer;
   private endVectorLayer: VectorLayer;
   private shouldLabelPoints = false;
+
+  ngOnInit() {
+    if (debugMode) {
+      this.initDebugMode();
+    }
+  }
+
+  private initDebugMode() {
+    this.onStartSvgTextLoaded(`
+      <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
+        <g transform="translate(12,12)">
+          <g transform="scale(0.75,0.75)">
+            <g transform="translate(-12,-12)">
+              <path d="M 0 0 L 4 4 C 11 12 17 12 24 12" stroke="#000" stroke-width="1" />
+            </g>
+          </g>
+        </g>
+      </svg>`);
+    this.onEndSvgTextLoaded(`
+      <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
+        <g transform="translate(12,12)">
+          <g transform="scale(0.75,0.75)">
+            <g transform="translate(-12,-12)">
+              <path d="M 0 0 L 12 12 C 16 16 20 20 24 24" stroke="#000" stroke-width="1" />
+            </g>
+          </g>
+        </g>
+      </svg>`);
+  }
 
   private animatePreviewVectorLayer(fraction: number): VectorLayer {
     const animateLayer = layer => {
