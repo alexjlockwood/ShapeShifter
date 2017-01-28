16b59028b6b2b0b8edc437008b720938984c37a3
Alex Lockwood <alexjlockwood@gmail.com>
2017-01-27 21:39:06 -0500

    fix

---
+++ src/app/scripts/common/pathhelper.spec.ts
@@ -0,0 +1,20 @@
+import { } from 'jasmine';
+import { PathHelper, Projection, createPathHelper } from './pathhelper';
+import { Point } from './mathutil';
+
+describe('PathHelper', () => {
+  it('constructor', () => {
+    expect([]).toEqual([]);
+  });
+});
+
+function makePoints(...nums: number[]) {
+  if (nums.length % 2 !== 0) {
+    throw new Error('Invalid number of points');
+  }
+  const points = [];
+  for (let i = 0; i < nums.length; i += 2) {
+    points.push(new Point(nums[i], nums[i + 1]));
+  }
+  return points;
+}
