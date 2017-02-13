93e759bc8d82c0278a406a319b94953ea71d7245
Alex Lockwood <alexjlockwood@gmail.com>
2017-02-13 14:07:07 -0500

    disable settings when not morphable

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -2,16 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { MdInputDirective } from '@angular/material';
 import { AnimatorService } from '../services/animator.service';
 import { Interpolator, INTERPOLATORS } from '../scripts/animation';
+import { LayerStateService, MorphabilityStatus } from '../services/layerstate.service';
+import { CanvasType } from '../CanvasType';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
   interpolators = INTERPOLATORS;
+  isMorphable = false;
 
-  constructor(private animatorService: AnimatorService) { }
+  constructor(
+    private animatorService: AnimatorService,
+    private layerStateService: LayerStateService) { }
+
+  ngOnInit() {
+    this.layerStateService.addListener(CanvasType.Start, event => {
+      this.isMorphable = event.morphabilityStatus === MorphabilityStatus.Morphable;
+    });
+    this.layerStateService.addListener(CanvasType.End, event => {
+      this.isMorphable = event.morphabilityStatus === MorphabilityStatus.Morphable;
+    });
+  }
 
   get selectedInterpolator() {
     return this.animatorService.getInterpolator();
