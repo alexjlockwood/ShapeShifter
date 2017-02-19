bbee830e5902f017e9b1f44d9b5f0f044ae63d08
Alex Lockwood <alexjlockwood@gmail.com>
2017-02-19 14:33:10 -0500

    fix karma

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     angularCli: {
-      config: './angular-cli.json',
+      config: './.angular-cli.json',
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
