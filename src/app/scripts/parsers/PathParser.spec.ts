158497ffdbce7e88194e1ffb39c3e84b13160e31
Alex Lockwood <alexjlockwood@gmail.com>
2017-02-22 20:32:34 -0500

    Fix SVG parsing bug with multiple relative move commands
    Fixes #22 and partially fixes #20.

---
+++ src/app/scripts/parsers/PathParser.spec.ts
@@ -0,0 +1,33 @@
+import 'jasmine';
+import { PathParser } from '.';
+import { Command } from '../commands';
+
+describe('PathParser', () => {
+  it(`Sub paths begin with lowercase 'm'`, () => {
+    const path = 'm 9 7 -1 1 -8 -8 L 10 10 Z';
+    const cmds = PathParser.parseCommands(path);
+    const actual = cmds.map(cmd => commandToString(cmd)).join(' ');
+    expect(actual).toEqual('M 9 7 L 8 8 L 0 0 L 10 10 Z');
+  });
+
+  it(`Sub paths begin with lowercase 'm'`, () => {
+    const path = 'm -1 1 -15 0 0 -2 15 0 Z';
+    const cmds = PathParser.parseCommands(path);
+    const actual = cmds.map(cmd => commandToString(cmd)).join(' ');
+    expect(actual).toEqual('M -1 1 L -16 1 L -16 -1 L -1 -1 Z');
+  });
+
+  it(`Sub paths begin with lowercase 'm'`, () => {
+    const path = 'm 9 7 -1 1 -8 -8 L 10 10 Z m -1 1 -15 0 0 -2 15 0 Z';
+    const cmds = PathParser.parseCommands(path);
+    const actual = cmds.map(cmd => commandToString(cmd)).join(' ');
+    expect(actual).toEqual('M 9 7 L 8 8 L 0 0 L 10 10 Z M 8 8 L -7 8 L -7 6 L 8 6 Z');
+  });
+});
+
+function commandToString(c: Command) {
+  if (c.svgChar === 'Z') {
+    return 'Z';
+  }
+  return `${c.svgChar} ${c.end.x} ${c.end.y}`;
+}
