5479109130614451bea379cf74c9256fcd295e7e
Alex Lockwood <alexjlockwood@gmail.com>
2017-03-21 01:15:04 -0400

    fix lint

--- src/app/scripts/commands/PathUtil.ts
+++ src/app/scripts/commands/PathUtil.ts
@@ -7,5 +7,5 @@ export function sortPathOps(ops: Array<{ subIdx: number, cmdIdx: number }>) {
       // Perform higher index splits first so that we don't alter the
       // indices of the lower index split operations.
       return s1 !== s2 ? s2 - s1 : c2 - c1;
-    });;
+    });
 }
