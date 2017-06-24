eea9b8b9e97e657be267ca78615416e609ec3da3
Alex Lockwood <alexjlockwood@gmail.com>
2017-06-24 14:57:20 -0400

    add dummy scroll group test

---
+++ src/app/components/scrollgroup/scrollgroup.directive.spec.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {
+  ComponentFixture,
+  TestBed,
+  async,
+  inject,
+} from '@angular/core/testing';
+
+import { ScrollGroupDirective } from './scrollgroup.directive';
+
+describe('SplitterComponent', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async(() => {
+    TestBed
+      .configureTestingModule({
+        declarations: [TestComponent, ScrollGroupDirective],
+      })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+@Component({
+  template: `<div appScrollGroup='test'></div>`,
+})
+class TestComponent { }
