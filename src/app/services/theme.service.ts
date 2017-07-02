65177500c41e0882bad884825b78b2b25adc2401
Alex Lockwood <alexjlockwood@gmail.com>
2017-07-02 19:19:20 -0400

    cleanup

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
 import { State, Store } from 'app/store';
 import { SetTheme } from 'app/store/theme/actions';
 import { isDarkTheme } from 'app/store/theme/selectors';
-import { OutputSelector } from 'reselect';
 
 /**
  * A simple service that provides an interface for making theme changes.
@@ -13,17 +12,13 @@ import { OutputSelector } from 'reselect';
 export class ThemeService {
   constructor(private readonly store: Store<State>) {}
 
-  isDarkTheme() {
-    return this.queryStore(isDarkTheme);
-  }
-
   toggleTheme() {
     this.store.dispatch(new SetTheme(!this.isDarkTheme()));
   }
 
-  private queryStore<T>(selector: OutputSelector<Object, T, (res: Object) => T>) {
-    let obj: T;
-    this.store.select(selector).first().subscribe(o => (obj = o));
-    return obj;
+  isDarkTheme() {
+    let result: boolean;
+    this.store.select(isDarkTheme).first().subscribe(res => (result = res));
+    return result;
   }
 }
