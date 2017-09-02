442d10e2b281d61046176524f288bb3816906c16
Alex Lockwood <alexjlockwood@gmail.com>
2017-09-02 08:38:31 -0400

    fix circular deps

--- src/app/model/paths/index.ts
+++ src/app/model/paths/index.ts
@@ -2,7 +2,6 @@ import * as PathUtil from './PathUtil';
 export { PathUtil };
 export { Projection, Line } from './calculators';
 export { SvgChar } from './SvgChar';
-export { Path, HitOptions, HitResult, ProjectionOntoPath } from './Path';
+export { Path, HitOptions, HitResult, ProjectionOntoPath, PathMutator } from './Path';
 export { SubPath } from './SubPath';
 export { Command } from './Command';
-export { PathMutator } from './PathMutator';
