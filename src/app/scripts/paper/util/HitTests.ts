3c0b5d3f3ce651a57203bed73afc6bf6574e7746
Alex Lockwood <alexjlockwood@gmail.com>
2017-09-13 16:32:57 -0400

    cleanup

--- src/app/scripts/paper/util/HitTests.ts
+++ src/app/scripts/paper/util/HitTests.ts
@@ -1,15 +1,6 @@
 import * as paper from 'paper';
 
 /**
- * Executes a hit test in search of selection bound pivots.
- */
-export function selectionBoundsPivot(selectionBounds: paper.Path.Rectangle, point: paper.Point) {
-  return selectionBounds.hitTest(point, {
-    segments: true,
-  });
-}
-
-/**
  * Executes a standard selection mode hit test.
  */
 export function selectionMode(point: paper.Point, matchFn?: (result: paper.HitResult) => boolean) {
@@ -17,24 +8,5 @@ export function selectionMode(point: paper.Point, matchFn?: (result: paper.HitRe
     fill: true,
     stroke: true,
     match: matchFn,
-  });
-}
-
-/**
- * Executes a standard edit path mode hit test.
- */
-export function editPathMode(selectedEditPath: paper.Path, point: paper.Point) {
-  return selectedEditPath.hitTest(point, {
-    segments: true,
-    curves: true,
-    handles: true,
-    stroke: true,
-    match: (hitResult: paper.HitResult) => {
-      // Don't return hits for handles belonging to un-selected segments.
-      return !(
-        (hitResult.type === 'handle-in' || hitResult.type === 'handle-out') &&
-        !hitResult.segment.selected
-      );
-    },
   });
 }
