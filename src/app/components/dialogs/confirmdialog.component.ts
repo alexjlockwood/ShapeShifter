e26ef3b05b05bcf0de37543f83dfc9a371a2ae46
Alex Lockwood <alexjlockwood@gmail.com>
2017-09-22 14:04:10 -0400

    revert some material changes

--- src/app/components/dialogs/confirmdialog.component.ts
+++ src/app/components/dialogs/confirmdialog.component.ts
@@ -1,25 +1,25 @@
 import { Inject } from '@angular/core';
 import { Component } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { MD_DIALOG_DATA, MdDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-confirmdialog',
   template: `
-  <span matDialogTitle>{{ this.data.title }}</span>
-  <mat-dialog-content>
+  <span mdDialogTitle>{{ this.data.title }}</span>
+  <md-dialog-content>
     <p>{{ this.data.message }}</p>
-  </mat-dialog-content>
-  <mat-dialog-actions fxLayout="row">
+  </md-dialog-content>
+  <md-dialog-actions fxLayout="row">
     <!-- The ordering here matters (it ensures that 'OK' gets focus first). -->
     <span fxFlex></span>
-    <button fxFlexOrder="2" mat-button (click)="dialogRef.close(true)">OK</button>
-    <button fxFlexOrder="1" mat-button matDialogClose>Cancel</button>
-  </mat-dialog-actions>`,
+    <button fxFlexOrder="2" md-button (click)="dialogRef.close(true)">OK</button>
+    <button fxFlexOrder="1" md-button mdDialogClose>Cancel</button>
+  </md-dialog-actions>`,
   styleUrls: ['./confirmdialog.component.scss'],
 })
 export class ConfirmDialogComponent {
   constructor(
-    public readonly dialogRef: MatDialogRef<ConfirmDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public readonly data: { title: string; message: string },
+    public readonly dialogRef: MdDialogRef<ConfirmDialogComponent>,
+    @Inject(MD_DIALOG_DATA) public readonly data: { title: string; message: string },
   ) {}
 }
