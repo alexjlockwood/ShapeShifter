944cba218ef5c44de6f1b62c0104126af3523a5c
Toufic Batache <taffou2a@gmail.com>
2017-10-25 23:21:51 +0300

    Update index.ts

--- src/app/scripts/demos/index.ts
+++ src/app/scripts/demos/index.ts
@@ -6,6 +6,7 @@ export interface DemoInfo {
 export const DEMO_INFOS: ReadonlyArray<DemoInfo> = [
   { id: 'playtopause', title: 'Play-to-pause' },
   { id: 'searchtoclose', title: 'Search-to-close' },
+  { id: 'menutoback', title: 'Menu-to-back' },
   { id: 'morphinganimals', title: 'Morphing animals' },
   { id: 'visibilitystrike', title: 'Visibility strike' },
   { id: 'heartbreak', title: 'Heart break' },
