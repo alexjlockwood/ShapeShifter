7fd05852509a5fc6ab514fac0752d1b3420920db
Alex Lockwood <alexjlockwood@gmail.com>
2017-11-12 12:00:08 -0800

    groups

--- src/app/scripts/paper/tool/gesture/HoverItemsGesture.ts
+++ src/app/scripts/paper/tool/gesture/HoverItemsGesture.ts
@@ -1,3 +1,4 @@
+import { Layer } from 'app/model/layers';
 import { ToolMode } from 'app/model/paper';
 import { HitResult, HitTests } from 'app/scripts/paper/item';
 import { Cursor, CursorUtil, PivotType } from 'app/scripts/paper/util';
@@ -6,6 +7,22 @@ import * as _ from 'lodash';
 import * as paper from 'paper';
 
 import { Gesture } from './Gesture';
+
+// prettier-ignore
+const RESIZE_CURSOR_MAP: ReadonlyMap<PivotType, Cursor> = new Map([
+  ['bottomLeft', Cursor.Resize45], ['leftCenter', Cursor.Resize90],
+  ['topLeft', Cursor.Resize135], ['topCenter', Cursor.Resize0],
+  ['topRight', Cursor.Resize45], ['rightCenter', Cursor.Resize90],
+  ['bottomRight', Cursor.Resize135], ['bottomCenter', Cursor.Resize0],
+] as [PivotType, Cursor][]);
+
+// prettier-ignore
+const ROTATE_CURSOR_MAP: ReadonlyMap<PivotType, Cursor> = new Map([
+  ['bottomLeft', Cursor.Rotate225], ['leftCenter', Cursor.Rotate270],
+  ['topLeft', Cursor.Rotate315], ['topCenter', Cursor.Rotate0],
+  ['topRight', Cursor.Rotate45], ['rightCenter', Cursor.Rotate90],
+  ['bottomRight', Cursor.Rotate135], ['bottomCenter', Cursor.Rotate180],
+] as [PivotType, Cursor][]);
 
 /**
  * A gesture that performs hover operations on items.
@@ -36,41 +53,39 @@ export class HoverItemsGesture extends Gesture {
     }
 
     const { children } = HitTests.selectionMode(event.point);
-    // TODO: make hit layer ID dependent on currently selected layers
-    const firstHitResult = _.find(children, function recurseFn(hitResult: HitResult) {
-      const firstChildHitResult = _.find(hitResult.children, recurseFn);
-      if (firstChildHitResult) {
-        return firstChildHitResult;
+    const selectionMap = this.getSelectedLayerMap();
+    let firstHitResult: HitResult;
+    _.forEach(children, function recurseFn(hitResult: HitResult) {
+      if (firstHitResult) {
+        return false;
       }
-      if (hitResult.hitItem && selectedLayers.has(hitResult.hitItem.data.id)) {
+      if (!selectionMap.get(hitResult.hitItem.data.id)) {
+        firstHitResult = hitResult;
+        return false;
       }
+      _.forEach(hitResult.children, recurseFn);
+      return true;
     });
-    if (firstHitResult && !selectedLayers.has(firstHitResult.hitItem.data.id)) {
+    if (firstHitResult) {
       this.ps.setHoveredLayer(firstHitResult.hitItem.data.id);
     } else {
       this.ps.setHoveredLayer(undefined);
     }
   }
+
+  /**
+   * Returns a map of layerIds to booleans. Each key-value pair indicates whether
+   * the subtree rooted at layerId contains a selected layer.
+   */
+  private getSelectedLayerMap() {
+    const map = new Map<string, boolean>();
+    const selectedLayers = this.ps.getSelectedLayers();
+    (function containsSelectedLayerFn(layer: Layer) {
+      let result = selectedLayers.has(layer.id);
+      layer.children.forEach(c => (result = containsSelectedLayerFn(c) || result));
+      map.set(layer.id, result);
+      return result;
+    })(this.ps.getVectorLayer());
+    return map;
+  }
 }
-
-const RESIZE_CURSOR_MAP: ReadonlyMap<PivotType, Cursor> = new Map([
-  ['bottomLeft', Cursor.Resize45],
-  ['leftCenter', Cursor.Resize90],
-  ['topLeft', Cursor.Resize135],
-  ['topCenter', Cursor.Resize0],
-  ['topRight', Cursor.Resize45],
-  ['rightCenter', Cursor.Resize90],
-  ['bottomRight', Cursor.Resize135],
-  ['bottomCenter', Cursor.Resize0],
-] as [PivotType, Cursor][]);
-
-const ROTATE_CURSOR_MAP: ReadonlyMap<PivotType, Cursor> = new Map([
-  ['bottomLeft', Cursor.Rotate225],
-  ['leftCenter', Cursor.Rotate270],
-  ['topLeft', Cursor.Rotate315],
-  ['topCenter', Cursor.Rotate0],
-  ['topRight', Cursor.Rotate45],
-  ['rightCenter', Cursor.Rotate90],
-  ['bottomRight', Cursor.Rotate135],
-  ['bottomCenter', Cursor.Rotate180],
-] as [PivotType, Cursor][]);
