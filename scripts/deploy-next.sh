643ea2cf1da8a2573aba3439a7fbc0d503cdf430
Alex Lockwood <alockwood@lyft.com>
2018-05-25 14:18:05 -0700

    update build scripts

---
+++ scripts/deploy-next.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+rm -rf dist
+git checkout next
+npm i
+git clone -b gh-pages --single-branch https://github.com/alexjlockwood/ShapeShifter dist
+ng build --prod --output-path 'dist/next' --base-href '/next/'
+ngh
