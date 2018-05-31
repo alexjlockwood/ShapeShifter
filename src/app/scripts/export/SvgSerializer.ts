baab19b4903214bbb81e493a650a74529e23d98a
Alex Lockwood <alockwood@lyft.com>
2018-05-31 10:45:44 -0700

    fixes #272

--- src/app/scripts/export/SvgSerializer.ts
+++ src/app/scripts/export/SvgSerializer.ts
@@ -74,7 +74,7 @@ function vectorLayerToSvgNode(
         (acc: ReadonlyArray<Entry>, curr) => {
           const seenClipPaths = acc.length ? [..._.last(acc).seenClipPaths] : [];
           // Ignore clip paths with empty path data strings.
-          if (curr instanceof ClipPathLayer && !!curr.pathData.getPathString()) {
+          if (curr instanceof ClipPathLayer && curr.pathData && curr.pathData.getPathString()) {
             clipPathToPathDataMap.set(curr.id, curr.pathData.getPathString());
             seenClipPaths.push(curr);
           }
@@ -147,7 +147,7 @@ function vectorLayerToSvgNode(
       }
       if (layer instanceof PathLayer) {
         const { pathData } = layer;
-        if (!pathData.getPathString()) {
+        if (!pathData || !pathData.getPathString()) {
           return undefined;
         }
         const node = xmlDoc.createElement('path');
