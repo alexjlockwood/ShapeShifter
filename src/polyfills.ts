7cee04fcab24851b4bec5ddd786e1d30b6f7789b
Alex Lockwood <alockwood@lyft.com>
2018-07-08 14:04:30 -0700

    fixes #295

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -26,20 +26,18 @@
 // import 'core-js/es6/parse-float';
 // import 'core-js/es6/number';
 // import 'core-js/es6/math';
-// import 'core-js/es6/string';
+import 'core-js/es6/string';
 // import 'core-js/es6/date';
-// import 'core-js/es6/array';
+import 'core-js/es6/array';
+import 'core-js/es7/array';
 // import 'core-js/es6/regexp';
 // import 'core-js/es6/map';
 // import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
-
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
-
 /** IE10 and IE11 requires the following for the Reflect API. */
 // import 'core-js/es6/reflect';
-
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 // TODO: comment this out before creating prod builds
