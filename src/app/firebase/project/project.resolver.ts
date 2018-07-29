96d6ca20da58cbfb090cdee45c903bd08ec06342
Alex Lockwood <alockwood@lyft.com>
2018-07-29 14:23:33 -0700

    creating a new project kinda works

--- src/app/firebase/project/project.resolver.ts
+++ src/app/firebase/project/project.resolver.ts
@@ -1,35 +1,86 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { Project, ProjectService } from 'app/components/project';
+import { Project } from 'app/components/project';
+import { AuthService } from 'app/firebase/core/auth.service';
 import { ModelUtil } from 'app/scripts/common';
 import { FileExportService } from 'app/services';
-import { from } from 'rxjs';
-import { first, map } from 'rxjs/operators';
+import { combineLatest, from, of } from 'rxjs';
+import { first, map, switchMap, tap, withLatestFrom } from 'rxjs/operators';
+
+// TODO: generate this server side instead?
+const defaultProjectContent = JSON.stringify({
+  version: 1,
+  layers: {
+    vectorLayer: {
+      id: '1',
+      name: 'myVector',
+      type: 'vector',
+      width: 24,
+      height: 24,
+      children: [] as string[],
+    },
+    hiddenLayerIds: [] as string[],
+  },
+  timeline: { animation: { id: '2', name: 'anim', duration: 300, blocks: [] as string[] } },
+});
+
+interface FirestoreProject {
+  readonly name: string;
+  readonly uid: string;
+  readonly content: string;
+}
 
 @Injectable()
 export class ProjectResolver implements Resolve<Project> {
   constructor(
     private readonly router: Router,
     private readonly angularFirestore: AngularFirestore,
+    private readonly authService: AuthService,
   ) {}
 
   resolve(route: ActivatedRouteSnapshot) {
     const id = route.paramMap.get('id');
-    const document = this.angularFirestore.doc(`projects/${id}`);
-    return document.valueChanges().pipe(
-      first(),
-      map((project: any) => {
-        // TODO: get rid of any type above
-        if (project) {
-          const { vectorLayer, animation, hiddenLayerIds } = FileExportService.fromJSON(
-            JSON.parse(project.content),
-          );
-          return ModelUtil.regenerateModelIds(vectorLayer, animation, hiddenLayerIds) as Project;
-        }
-        this.router.navigate(['/login']);
-        return undefined;
-      }),
+    const observeUserUid = this.authService.observeUser().pipe(map(user => user.uid));
+    const observeDocPayload = this.angularFirestore
+      .doc<FirestoreProject>(`projects/${id}`)
+      .snapshotChanges()
+      .pipe(map(action => action.payload));
+    return combineLatest(observeUserUid, observeDocPayload)
+      .pipe(
+        switchMap(([uid, payload]) => {
+          if (!payload.exists) {
+            const firestoreProject = {
+              id,
+              uid,
+              name: 'my new project',
+              content: defaultProjectContent,
+            };
+            return from<Project>(
+              this.angularFirestore
+                .collection<FirestoreProject>('projects')
+                .doc(id)
+                .set(firestoreProject)
+                .then(() => {
+                  console.log('success');
+                  return this.fromFirestoreProject(firestoreProject);
+                })
+                .catch(() => {
+                  console.log('error');
+                  return undefined;
+                }),
+            );
+          }
+          return of(this.fromFirestoreProject(payload.data()));
+        }),
+      )
+      .pipe(first());
+  }
+
+  private fromFirestoreProject(firestoreProject: FirestoreProject) {
+    const { vectorLayer, animation, hiddenLayerIds } = FileExportService.fromJSON(
+      JSON.parse(firestoreProject.content),
     );
+    return ModelUtil.regenerateModelIds(vectorLayer, animation, hiddenLayerIds) as Project;
   }
 }
--- src/app/firebase/projectlist/projectlist.component.ts
+++ src/app/firebase/projectlist/projectlist.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AngularFirestore } from 'angularfire2/firestore';
 import { AuthService } from 'app/firebase/core/auth.service';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-interface Project {
+interface FirestoreProject {
   readonly id: string;
   readonly name: string;
   readonly uid: string;
@@ -18,24 +18,29 @@ interface Project {
   styleUrls: ['./projectlist.component.scss'],
 })
 export class ProjectListComponent {
-  private readonly projectsCollection: AngularFirestoreCollection<Project>;
-  readonly projects: Observable<Project[]>;
+  readonly projects: Observable<FirestoreProject[]>;
 
   constructor(
-    angularFirestore: AngularFirestore,
+    private readonly angularFirestore: AngularFirestore,
     private readonly authService: AuthService,
     private readonly router: Router,
   ) {
-    this.projectsCollection = angularFirestore.collection<Project>('projects');
-    this.projects = this.projectsCollection.snapshotChanges().pipe(
-      map(actions => {
-        return actions.map(action => {
-          const project = action.payload.doc.data() as Project;
-          const id = action.payload.doc.id;
-          return { id, ...project };
-        });
-      }),
-    );
+    this.projects = angularFirestore
+      .collection<FirestoreProject>('projects')
+      .snapshotChanges()
+      .pipe(
+        map(actions => {
+          return actions.map(action => {
+            const project = action.payload.doc.data() as FirestoreProject;
+            const id = action.payload.doc.id;
+            return { id, ...project };
+          });
+        }),
+      );
+  }
+
+  newProject() {
+    this.router.navigateByUrl(`/project/${this.angularFirestore.createId()}`);
   }
 
   signOut() {
