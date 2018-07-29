b4ec7e922f0d2ee7fa70e6931cbf55abe8801642
Alex Lockwood <alockwood@lyft.com>
2018-07-29 15:05:36 -0700

    remove unused imports

--- src/app/firebase/app.module.ts
+++ src/app/firebase/app.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AppModule as ProjectEditorModule } from 'app/app.module';
 import { AppRoutingModule } from 'app/firebase/app-routing.module';
 import { CoreModule } from 'app/firebase/core/core.module';
 import { ProjectListComponent } from 'app/firebase/projectlist/projectlist.component';
-import { environment } from 'environments/environment';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
