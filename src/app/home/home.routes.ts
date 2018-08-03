575790b3b61209cb35e7e2474bd7e07d23b7e083
Alex Lockwood <alockwood@lyft.com>
2018-08-02 19:37:53 -0700

    remove unneeded route guard

--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from 'app/core';
 
 import { HomeComponent } from './home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent, canActivate: [AuthGuard] }];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
