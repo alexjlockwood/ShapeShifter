17d3b65c512202605093896f757a1edde7f0cfdc
Alex Lockwood <alockwood@lyft.com>
2018-08-04 13:41:55 -0700

    fix crash

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import 'hammerjs';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { EditorModule } from 'app';
+import { EditorModule } from 'app/editor/editor.module';
 import { environment } from 'environments/environment';
 
 const script = document.createElement('script');
