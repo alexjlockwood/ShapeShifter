f55f321d77728ce37ee6cbd3fb9e59fb6a3bfe22
Alex Lockwood <alockwood@lyft.com>
2018-08-18 14:41:59 -0700

    refactor

--- src/app/core/store/auth/auth.effects.ts
+++ src/app/core/store/auth/auth.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
-import { Actions, Effect } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { SigninDialogComponent } from 'app/shared/components/signin-dialog';
@@ -28,7 +28,8 @@ import {
 @Injectable()
 export class AuthEffects {
   @Effect()
-  showSigninDialog$ = this.actions$.ofType<ShowSigninDialog>(AuthActionTypes.ShowSigninDialog).pipe(
+  showSigninDialog$ = this.actions$.pipe(
+    ofType<ShowSigninDialog>(AuthActionTypes.ShowSigninDialog),
     exhaustMap(() =>
       this.matDialog
         .open<SigninDialogComponent, undefined, boolean>(SigninDialogComponent)
@@ -38,48 +39,45 @@ export class AuthEffects {
   );
 
   @Effect({ dispatch: false })
-  signinDialogConfirmed$ = this.actions$
-    .ofType<SigninDialogConfirmed>(AuthActionTypes.SigninDialogConfirmed)
-    .pipe(
-      exhaustMap(() =>
-        this.signInWithGoogle().pipe(
-          map(() => new SigninSuccess()),
-          catchError(error => {
-            console.error('Unable to sign in', error);
-            return of(new SigninFailure());
-          }),
-        ),
+  signinDialogConfirmed$ = this.actions$.pipe(
+    ofType<SigninDialogConfirmed>(AuthActionTypes.SigninDialogConfirmed),
+    exhaustMap(() =>
+      this.signInWithGoogle().pipe(
+        map(() => new SigninSuccess()),
+        catchError(error => {
+          console.error('Unable to sign in', error);
+          return of(new SigninFailure());
+        }),
       ),
-    );
+    ),
+  );
 
   @Effect()
-  showSignoutDialog$ = this.actions$
-    .ofType<ShowSignoutDialog>(AuthActionTypes.ShowSignoutDialog)
-    .pipe(
-      exhaustMap(() =>
-        this.matDialog
-          .open<SignoutDialogComponent, undefined, boolean>(SignoutDialogComponent)
-          .beforeClose()
-          .pipe(map(res => (res ? new SignoutDialogConfirmed() : new SignoutDialogCanceled()))),
-      ),
-    );
+  showSignoutDialog$ = this.actions$.pipe(
+    ofType<ShowSignoutDialog>(AuthActionTypes.ShowSignoutDialog),
+    exhaustMap(() =>
+      this.matDialog
+        .open<SignoutDialogComponent, undefined, boolean>(SignoutDialogComponent)
+        .beforeClose()
+        .pipe(map(res => (res ? new SignoutDialogConfirmed() : new SignoutDialogCanceled()))),
+    ),
+  );
 
   @Effect({ dispatch: false })
-  signoutDialogConfirmed$ = this.actions$
-    .ofType<SignoutDialogConfirmed>(AuthActionTypes.SignoutDialogConfirmed)
-    .pipe(
-      exhaustMap(() =>
-        from(this.angularFireAuth.auth.signOut()).pipe(
-          map(() => new SignoutSuccess()),
-          // TODO: figure out if we should redirect in this case
-          tap(() => this.router.navigate(['/'])),
-          catchError(error => {
-            console.error('Unable to sign out', error);
-            return of(new SignoutFailure());
-          }),
-        ),
+  signoutDialogConfirmed$ = this.actions$.pipe(
+    ofType<SignoutDialogConfirmed>(AuthActionTypes.SignoutDialogConfirmed),
+    exhaustMap(() =>
+      from(this.angularFireAuth.auth.signOut()).pipe(
+        map(() => new SignoutSuccess()),
+        // TODO: figure out if we should redirect in this case
+        tap(() => this.router.navigate(['/'])),
+        catchError(error => {
+          console.error('Unable to sign out', error);
+          return of(new SignoutFailure());
+        }),
       ),
-    );
+    ),
+  );
 
   constructor(
     private readonly actions$: Actions,
